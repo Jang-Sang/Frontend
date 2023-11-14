5cd033e6f7aae3f5719f17de15765dd2b1ee5612
Jang-Sang <wkdtkdwns2@gmail.com>
2023-11-14 18:51:05 +0900

    todolist

---
+++ React/todomaster/todolist 0716/src/routes/router.js
@@ -0,0 +1,11 @@
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage/>,
+    },
+    {
+      path: "/",
+      element: <TodoPage/>,
+    }
+  ]);
+  export default router
