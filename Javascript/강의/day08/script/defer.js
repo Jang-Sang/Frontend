febf747d406c0708eabdfdc9841a2fe9deb3b8a5
Jang-Sang <wkdtkdwns2@gmail.com>
2023-12-29 21:24:08 +0900

    8일차

---
+++ Javascript/강의/day08/script/defer.js
@@ -0,0 +1,8 @@
+"use strict";
+//'use client' -> nextJS에서 사용하는 방법
+
+const idDom = document.getElementById("id");
+const idSelector = document.querySelector("#id");
+console.log(idDom, idSelector);
+
+// 외부에서 자바스크립트를 불러오는 방법
